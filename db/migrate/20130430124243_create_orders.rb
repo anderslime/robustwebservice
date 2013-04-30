406915e5edbda96abab77473dfec87efb28c8932
gottelicious <t_macnr1@hotmail.com>
2013-04-30 14:44:22 +0200

    order model

---
+++ db/migrate/20130430124243_create_orders.rb
@@ -0,0 +1,12 @@
+class CreateOrders < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.datetime :date
+      t.string :paymentinfo
+      t.references :customer
+
+      t.timestamps
+    end
+    add_index :orders, :customer_id
+  end
+end
