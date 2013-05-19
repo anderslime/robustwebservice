ffc76152085dc21768617a7f055569b8eb59c426
anderslime <aemilnielsen@gmail.com>
2013-05-19 16:34:52 +0200

    A customer can delete and see details of order

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -1,6 +1,10 @@
 class Customer::OrdersController < CustomerController
   before_filter :authenticate_customer_with_token
 
+  def show
+    respond_with Order.find(params[:id])
+  end
+
   def index
     respond_with Order.where(customer_id: current_customer)
   end
@@ -8,6 +12,11 @@ class Customer::OrdersController < CustomerController
   def create
     order = OrderService.new(order_params, current_customer).create
     respond_with order, :status => :created
+  end
+
+  def destroy
+    Order.find(params[:id]).destroy
+    render :nothing => true, :status => :ok
   end
 
   private
