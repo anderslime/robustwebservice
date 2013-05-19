b4055a9701fa13646577742c8c62c13003fd0774
anderslime <aemilnielsen@gmail.com>
2013-05-19 16:46:07 +0200

    Welcome screen

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,11 @@
 class WelcomeController < ApplicationController
+  respond_to :html, :json
   def index
     render :json => {
       :welcome => true,
-      :documentation => "link/to/documenation",
-      :report => "link/to/report"
+      :documentation => "#{request.protocol}#{request.host}/readme.html",
+      :source_code => "https://github.com/anderslime/robustwebservice",
+      :report => "#{request.protocol}#{request.host}/report.pdf"
     }
   end
 end
