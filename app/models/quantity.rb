ea6d39340d667c9991562df123b403292a9425be
anderslime <aemilnielsen@gmail.com>
2013-05-19 12:31:02 +0200

    Fixed validations on quantity

--- app/models/quantity.rb
+++ app/models/quantity.rb
@@ -3,5 +3,5 @@ class Quantity < ActiveRecord::Base
   belongs_to :product
   attr_accessible :quantity, :order_id, :product_id
 
-  validates_presence_of :order_id, :product_id, :quantity
+  validates_presence_of :product_id, :quantity
 end
