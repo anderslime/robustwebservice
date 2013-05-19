f16072c2477732ec07b268304ef188c68623f523
anderslime <aemilnielsen@gmail.com>
2013-05-19 15:26:43 +0200

    Render json to be able to show in browser

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,9 @@
 class ProductsController < ApplicationController
   def index
-    respond_with Product.all
+    render :json => Product.all
   end
 
   def show
-    respond_with Product.find(params[:id])
+    render :json => Product.find(params[:id])
   end
 end
