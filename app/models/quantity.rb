af1b46e39bf7b27924c73cfdba615d6a9077d6b0
anderslime <aemilnielsen@gmail.com>
2013-05-19 11:09:01 +0200

    Quantity validations

--- app/models/quantity.rb
+++ app/models/quantity.rb
@@ -1,5 +1,7 @@
 class Quantity < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
-  attr_accessible :quantity
+  attr_accessible :quantity, :order_id, :product_id
+
+  validates_presence_of :order_id, :product_id, :quantity
 end
